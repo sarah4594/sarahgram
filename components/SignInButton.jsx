412e759a9ed7d289393719ab52b9ebe7ede3c928
sarah4594 <sarah4594@gmail.com>
2020-04-19 12:28:55 -0500

    ⚡️Add SignInButton component

---
+++ components/SignInButton.jsx
@@ -0,0 +1,15 @@
+const SignInButton = ({ children, onClick }) => (
+  <div>
+    <span className="w-full inline-flex rounded-md shadow-sm">
+      <button
+        type="button"
+        className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md bg-white text-sm leading-5 font-medium text-gray-500 hover:text-gray-400 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition duration-150 ease-in-out"
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    </span>
+  </div>
+)
+
+export default SignInButton
