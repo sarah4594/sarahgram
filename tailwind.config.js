231919b5639031049cca95638893ae665130304c
sarah4594 <sarah4594@gmail.com>
2020-05-01 22:08:38 -0500

    ⚙️Update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   theme: {
     extend: {
+      colors: {
+        primary: {
+          '50': '#f6f5ff',
+          '100': '#edebfe',
+          '200': '#dcd7fe',
+          '300': '#cabffd',
+          '400': '#ac94fa',
+          '500': '#9061f9',
+          '600': '#7e3af2',
+          '700': '#6c2bd9',
+          '800': '#5521b5',
+          '900': '#4a1d96',
+        },
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
