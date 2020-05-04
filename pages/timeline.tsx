5c5c11fc3dbf440fac84ef194010ec2bfcc5e4cb
sarah4594 <sarah4594@gmail.com>
2020-05-04 10:47:14 -0500

    🚧✨Start timeline page

---
+++ pages/timeline.tsx
@@ -0,0 +1,44 @@
+import '../css/main.css'
+import React from 'react'
+import AppShell from '../components/app/AppShell'
+import Footer from '../components/app/footer'
+
+const Timeline = () => {
+  return (
+    <AppShell>
+      <div className="grid grid-cols-2 gap-2">
+        <div className="border-4 border-red-500 rounded-lg">
+          <p>This is the timeline</p>
+        </div>
+        <div className="grid grid-cols gap-4">
+          <div className="border-4 border-green-500 rounded-lg">
+            <img
+              className="h-8 w-8 rounded-full"
+              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+            These are stories
+            <img
+              className="h-8 w-8 rounded-full"
+              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+            <img
+              className="h-8 w-8 rounded-full"
+              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+            <img
+              className="h-8 w-8 rounded-full"
+              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </AppShell>
+  )
+}
+
+export default Timeline
