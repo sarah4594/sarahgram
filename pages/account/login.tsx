9c3067057317dcfe8a4bebb4d859a1b33c7941b4
sarah4594 <sarah4594@gmail.com>
2020-05-04 10:47:47 -0500

    🔥Get rid of app shell on login page

--- pages/account/login.tsx
+++ pages/account/login.tsx
@@ -94,7 +94,7 @@ function Login() {
   }, []) // [] = run once
 
   return (
-    <AppShell>
+    <>
       <div>
         <div className="min-h-screen bg-white flex">
           <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
@@ -204,7 +204,7 @@ function Login() {
           </div>
         </div>
       </div>
-    </AppShell>
+    </>
   )
 }
 
