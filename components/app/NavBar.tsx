b38eb8e7cac6d7f8b0656dad27d21fa1a23275ba
sarah4594 <sarah4594@gmail.com>
2020-05-20 12:25:54 -0500

    🚧✨Add support to upload photos

--- components/app/NavBar.tsx
+++ components/app/NavBar.tsx
@@ -31,6 +31,11 @@ const NavBar = (props: any) => {
     Router.push('/timeline')
   }
 
+  const goToUploadPhotos = (e: any) => {
+    e.preventDefault()
+    Router.push('/addPicture')
+  }
+
   const onclick = (e: any) => {
     e.preventDefault()
   }
@@ -146,6 +151,7 @@ const NavBar = (props: any) => {
                   linejoin="round"
                   width="2"
                   stroke="currentColor"
+                  onClick={goToUploadPhotos}
                 >
                   <path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path>
                   <path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path>
