863f2f41f47fce501ceb953500eda944ae8cfa80
kiliman <kiliman@gmail.com>
2020-05-27 19:45:02 -0400

    🔨 Simplify update profile page

--- pages/account/update-info.tsx
+++ pages/account/update-info.tsx
@@ -10,13 +10,14 @@ import initFirebase from '../../utils/auth/initFirebase'
 import Footer from '../../components/app/footer'
 import AppShell from '../../components/app/AppShell'
 import Button from '../../components/elements/Button'
+import { useAuthUserInfo } from '../../utils/auth/hooks'
 
 initFirebase()
 
-const AccountUpdateName = (props: any) => {
-  const { AuthUserInfo } = props
-  var authUser = get(AuthUserInfo, 'AuthUser')
+const AccountUpdateName = () => {
   var input: HTMLInputElement | null = null
+
+  const { AuthUser } = useAuthUserInfo()
 
   const handleDisplayNameSubmit = async () => {
     try {
@@ -25,7 +26,6 @@ const AccountUpdateName = (props: any) => {
         await user.updateProfile({
           displayName: input?.value || '',
         })
-        authUser = user
       }
       Router.push('/account')
     } catch (error) {
@@ -34,11 +34,8 @@ const AccountUpdateName = (props: any) => {
   }
 
   useEffect(() => {
-    if (!authUser) {
-      Router.push('/')
-    }
     if (input) {
-      input.value = authUser?.displayName || ''
+      input.value = AuthUser?.displayName || ''
       input.focus()
     }
   })
@@ -49,36 +46,30 @@ const AccountUpdateName = (props: any) => {
   }
 
   return (
-    <>
-      {!authUser ? (
-        <></>
-      ) : (
-        <AppShell title="Update Info">
-          <div className="p-4">
-            <label htmlFor="displayName">Display Name </label>
-            <input
-              type="text"
-              id="displayName"
-              name="displayName"
-              ref={(r) => (input = r)}
-              defaultValue=""
-            />
-          </div>
-          {/* <div className="p-4">
+    <AppShell title="Update Info">
+      <div className="p-4">
+        <label htmlFor="displayName">Display Name </label>
+        <input
+          type="text"
+          id="displayName"
+          name="displayName"
+          ref={(r) => (input = r)}
+          defaultValue=""
+        />
+      </div>
+      {/* <div className="p-4">
             <label htmlFor="email">Email </label>
             <input type="email" id="email" name="email" defaultValue="" />
           </div> */}
-          <div className="grid cols-1 pb-2">
-            <Button onClick={handleDisplayNameSubmit} label="Update" />
-          </div>
-          <div className="grid cols-1 pb-2">
-            <Button onClick={gotoAccount} label="Back To Account" />
-          </div>
-          <Footer />
-        </AppShell>
-      )}
-    </>
+      <div className="grid pb-2 cols-1">
+        <Button onClick={handleDisplayNameSubmit} label="Update" />
+      </div>
+      <div className="grid pb-2 cols-1">
+        <Button onClick={gotoAccount} label="Back To Account" />
+      </div>
+      <Footer />
+    </AppShell>
   )
 }
 
-export default withAuthUser(withAuthUserInfo(AccountUpdateName))
+export default withAuthUser(AccountUpdateName)
