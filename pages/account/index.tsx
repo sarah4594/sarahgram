e144bcb3f53cbf0c123fe86c943a44b1fd532a19
sarah4594 <sarah4594@gmail.com>
2020-06-17 15:36:37 -0500

    ✨Add username to profile settings

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -64,6 +64,14 @@ const UserProfile = (props: any) => {
                   </div>
                   <div className="mt-8 sm:mt-0 sm:grid sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:px-6 sm:py-5">
                     <dt className="text-sm leading-5 font-medium text-gray-500">
+                      Username
+                    </dt>
+                    <dd className="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                      {authUser.username}
+                    </dd>
+                  </div>
+                  <div className="mt-8 sm:mt-0 sm:grid sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:px-6 sm:py-5">
+                    <dt className="text-sm leading-5 font-medium text-gray-500">
                       Id
                     </dt>
                     <dd className="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
